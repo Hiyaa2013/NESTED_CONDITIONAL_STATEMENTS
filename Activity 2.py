316805abd43422229b29d25758c0eb87c735be37
Hiyaa2013 <hiyaah.shimpi@gmail.com>
2025-11-14 19:34:51 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,15 @@
+unit=int(input("Please enter number of units you consume:  "))
+if unit<50:
+    amount=unit*2.60
+    subcharge=25
+elif unit<=100:
+    amount=130+((unit-50)*3.25)
+    subcharge=35
+elif unit<=200:
+    amount=130+162.50+((unit-100)*5.26)
+    subcharge=45
+else:
+    amount=130+162.50+526+((unit-200)*8.45)
+    subcharge=75
+total=amount+subcharge
+print("\nelectricity bill = %.2f"  %total)
